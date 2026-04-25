fcf73ad076c6826b978993b14c256af8ae87983c
Lse-Yeong <ocdee39@gmail.com>
2026-04-25 16:40:46 +0900

    feat : Cors 기능 추가

---
+++ PTDServer/src/main/java/com/example/myhomework/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.example.myhomework.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")          // 모든 경로
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
